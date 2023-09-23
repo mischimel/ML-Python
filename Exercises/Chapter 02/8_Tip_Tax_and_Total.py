c3ea5637ec4cfc79321eeff0f5199f0fd29adb23
mischimel <michele.fille@students.fhnw.ch>
2023-09-23 14:57:04 +0200

    Exercises CH2

--- Exercises/Chapter 02/8_Tip_Tax_and_Total.py
+++ Exercises/Chapter 02/8_Tip_Tax_and_Total.py
@@ -1,4 +1,16 @@
+# Write a program that calculates the total amount of a meal purchased at a restaurant.
+# The program should ask the user to enter the charge for the food,
+# then calculate the amounts of a 18 percent tip and 7 percent sales tax.
+# Display each of these amounts and the total.
 
+food_charge = float(input("Enter the charge for the food: "))
 
+tip = food_charge * 0.18
+sales_tax = food_charge * 0.07
 
+print("If the charge of your food is ", food_charge,
+      "\nthen 18% tip are ", round(tip,2) , "\nand the 7% sales tax are ", round(sales_tax,2) , "."
+      "\nThe total consiting of the charge of the food, the tip and the sales tax is " , round(food_charge+tip+sales_tax,2))
+
+# function round(number, decimals)
 
