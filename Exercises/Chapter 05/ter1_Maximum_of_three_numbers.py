b7d80bc76e32633289520955f35e5975e4da961b
mischimel <michele.fille@students.fhnw.ch>
2023-09-28 10:52:08 +0200

    Lesson 2 - CH5

--- Exercises/Chapter 05/ter1_Maximum_of_three_numbers.py
+++ Exercises/Chapter 05/ter1_Maximum_of_three_numbers.py
@@ -8,12 +8,13 @@ def maximum(num_1, num_2):
     else:
         return num_1
 
+
 # Main method
 def main():
-num_1 = int(input("Please enter number 1: "))
-num_2 = int(input("Please enter number 2: "))
-#num_3 = int(input("Please enter number 3: "))
-print(f"Maximum of the two numbers is: {maximum(num_1,num_2)}")
+    num_1 = int(input("Please enter number 1: "))
+    num_2 = int(input("Please enter number 2: "))
+    # num_3 = int(input("Please enter number 3: "))
+    print(f"Maximum of the two numbers is: {maximum(num_1, num_2)}")
 
 # Calling the main method
 main()
