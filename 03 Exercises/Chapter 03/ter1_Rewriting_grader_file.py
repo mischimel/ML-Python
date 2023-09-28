5b1dad33c0307b03ac8509c87ccb8b17abcf5a6d
mischimel <michele.fille@students.fhnw.ch>
2023-09-28 12:18:02 +0200

    Exercises CH3

---
+++ 03 Exercises/Chapter 03/ter1_Rewriting_grader_file.py
@@ -0,0 +1,2 @@
+# Rewrite grader.py (see chapter 3 programs) by using • Version 1: the elif statement
+# • Version 2: the match / case statement (is a “switch”-like statement available from v3.10 on)
