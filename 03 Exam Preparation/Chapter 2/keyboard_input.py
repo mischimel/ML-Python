8e0a95336b3d5992e404323dd1afe52d7f7d4fdf
mischimel <michele.fille@students.fhnw.ch>
2023-12-28 01:00:08 +0100

    Exam prep

--- 03 Exam Preparation/Chapter 2/keyboard_input.py
+++ 03 Exam Preparation/Chapter 2/keyboard_input.py
@@ -2,13 +2,10 @@
 
 # Using the input function to read a string input from the user
 name = input("Enter your name: ")
-print("Hello, " + name + "!")
 
 # Using the input function to read an integer input from the user
-age_str = int(input("Enter your age: "))
+age = int(input("Enter your age: ")) # Convert the string input to an integer using the int() function
 
-# Convert the string input to an integer using the int() function
-age = int(age_str)
 # Perform an operation with the user's age
 double_age = age * 2
-print("Your age doubled is:", double_age)
+
